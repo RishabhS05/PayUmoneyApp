8bb80b6f92519763fc6a5ac488380786b0cccefb
RishabhS05 <rishabhshrivastava.27@gmail.com>
2019-09-13 17:41:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/payumoneyapp/Constants.java
@@ -0,0 +1,9 @@
+package com.example.payumoneyapp;
+
+public class Constants {
+    public static final String EMAIL= "xyx@gmail.com";
+    public static final String PHONE ="9090909090";
+    public static final String AMOUNT ="500";
+    public static final String PRODUCT_NAME = "TEXT PRODUCT";
+    public static final String FIRST_NAME ="TEXT USER" ;
+}
---
+++ app/src/main/java/com/example/payumoneyapp/MainActivity.java
@@ -0,0 +1,334 @@
+package com.example.payumoneyapp;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.payumoney.core.PayUmoneyConfig;
+import com.payumoney.core.PayUmoneyConstants;
+import com.payumoney.core.PayUmoneySdkInitializer;
+import com.payumoney.core.entity.TransactionResponse;
+import com.payumoney.sdkui.ui.utils.PayUmoneyFlowManager;
+import com.payumoney.sdkui.ui.utils.ResultModel;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+
+    public static final String TAG = "MainActivity : ";
+    private boolean isDisableExitConfirmation = false;
+    private AppPreference mAppPreference;
+    private Button payNowButton;
+    private PayUmoneySdkInitializer.PaymentParam mPaymentParams;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAppPreference = new AppPreference();
+        payNowButton = findViewById(R.id.pay_now_button);
+        payNowButton.setOnClickListener(this);
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_main;
+    }
+
+
+    @Override
+    public void onClick(View view) {
+launchPayUMoneyFlow();
+    }
+    //-----------------------------------------
+    public static String hashCal(String str) {
+        byte[] hashseq = str.getBytes();
+        StringBuilder hexString = new StringBuilder();
+        try {
+            MessageDigest algorithm = MessageDigest.getInstance("SHA-512");
+            algorithm.reset();
+            algorithm.update(hashseq);
+            byte messageDigest[] = algorithm.digest();
+            for (byte aMessageDigest : messageDigest) {
+                String hex = Integer.toHexString(0xFF & aMessageDigest);
+                if (hex.length() == 1) {
+                    hexString.append("0");
+                }
+                hexString.append(hex);
+            }
+        } catch (NoSuchAlgorithmException ignored) {
+        }
+        return hexString.toString();
+    }
+    //------------------------------------------
+    private void launchPayUMoneyFlow() {
+
+        PayUmoneyConfig payUmoneyConfig = PayUmoneyConfig.getInstance();
+
+        //Use this to set your custom title for the activity
+        payUmoneyConfig.setPayUmoneyActivityTitle("PAYMENT");
+        payUmoneyConfig.disableExitConfirmation(isDisableExitConfirmation);
+
+        PayUmoneySdkInitializer.PaymentParam.Builder builder = new PayUmoneySdkInitializer.PaymentParam.Builder();
+
+        double amount = 0;
+        try {
+            amount = Double.parseDouble(Constants.AMOUNT);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String txnId = System.currentTimeMillis() + "";
+        //String txnId = "TXNID720431525261327973";
+        String phone = Constants.PHONE;
+        String productName = mAppPreference.getProductInfo();
+        String firstName = mAppPreference.getFirstName();
+        String email =Constants.EMAIL;
+        String udf1 = "";
+        String udf2 = "";
+        String udf3 = "";
+        String udf4 = "";
+        String udf5 = "";
+        String udf6 = "";
+        String udf7 = "";
+        String udf8 = "";
+        String udf9 = "";
+        String udf10 = "";
+
+        AppEnvironment appEnvironment = ((BaseApplication) getApplication()).getAppEnvironment();
+        builder.setAmount(String.valueOf(amount))
+                .setTxnId(txnId)
+                .setPhone(phone)
+                .setProductName(productName)
+                .setFirstName(firstName)
+                .setEmail(email)
+                .setsUrl(appEnvironment.surl())
+                .setfUrl(appEnvironment.furl())
+                .setUdf1(udf1)
+                .setUdf2(udf2)
+                .setUdf3(udf3)
+                .setUdf4(udf4)
+                .setUdf5(udf5)
+                .setUdf6(udf6)
+                .setUdf7(udf7)
+                .setUdf8(udf8)
+                .setUdf9(udf9)
+                .setUdf10(udf10)
+                .setIsDebug(appEnvironment.debug())
+                .setKey(appEnvironment.merchant_Key())
+                .setMerchantId(appEnvironment.merchant_ID());
+
+        try {
+            mPaymentParams = builder.build();
+
+            /*
+             * Hash should always be generated from your server side.
+             * */
+            //generateHashFromServer(mPaymentParams);
+
+            /*            *//**
+             * Do not use below code when going live
+             * Below code is provided to generate hash from sdk.
+             * It is recommended to generate hash from server side only.
+             * */
+            mPaymentParams = calculateServerSideHashAndInitiatePayment1(mPaymentParams);
+
+            if (AppPreference.selectedTheme != -1) {
+                PayUmoneyFlowManager.startPayUMoneyFlow(mPaymentParams,MainActivity.this, AppPreference.selectedTheme,mAppPreference.isOverrideResultScreen());
+            } else {
+                PayUmoneyFlowManager.startPayUMoneyFlow(mPaymentParams,MainActivity.this, R.style.AppTheme_default, mAppPreference.isOverrideResultScreen());
+            }
+
+        } catch (Exception e) {
+            // some exception occurred
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            payNowButton.setEnabled(true);
+        }
+    }
+//---------------------------
+
+    /**
+     * Thus function calculates the hash for transaction
+     *
+     * @param paymentParam payment params of transaction
+     * @return payment params along with calculated merchant hash
+     */
+    private PayUmoneySdkInitializer.PaymentParam calculateServerSideHashAndInitiatePayment1(final PayUmoneySdkInitializer.PaymentParam paymentParam) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        HashMap<String, String> params = paymentParam.getParams();
+        stringBuilder.append(params.get(PayUmoneyConstants.KEY) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.TXNID) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.AMOUNT) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.PRODUCT_INFO) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.FIRSTNAME) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.EMAIL) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.UDF1) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.UDF2) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.UDF3) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.UDF4) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.UDF5) + "||||||");
+
+        AppEnvironment appEnvironment = ((BaseApplication) getApplication()).getAppEnvironment();
+        stringBuilder.append(appEnvironment.salt());
+
+        String hash = hashCal(stringBuilder.toString());
+        paymentParam.setMerchantHash(hash);
+
+        return paymentParam;
+    }
+    //-----------------------------------------------------------------
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result Code is -1 send from Payumoney activity
+        Log.d("MainActivity", "request code " + requestCode + " resultcode " + resultCode);
+
+        if (requestCode == PayUmoneyFlowManager.REQUEST_CODE_PAYMENT && resultCode == RESULT_OK && data != null) {
+
+            TransactionResponse transactionResponse = data.getParcelableExtra(PayUmoneyFlowManager.INTENT_EXTRA_TRANSACTION_RESPONSE);
+            ResultModel resultModel = data.getParcelableExtra(PayUmoneyFlowManager.ARG_RESULT);
+
+            if (transactionResponse != null && transactionResponse.getPayuResponse() != null) {
+
+                if (transactionResponse.getTransactionStatus().equals(TransactionResponse.TransactionStatus.SUCCESSFUL)) {
+                    showAlert("Payment Successful");
+                } else if (transactionResponse.getTransactionStatus().equals(TransactionResponse.TransactionStatus.CANCELLED)) {
+                    showAlert("Payment Cancelled");
+                } else if (transactionResponse.getTransactionStatus().equals(TransactionResponse.TransactionStatus.FAILED)) {
+                    showAlert("Payment Failed");
+                }
+
+            } else if (resultModel != null && resultModel.getError() != null) {
+                Toast.makeText(this, "Error check log", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Both objects are null", Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == PayUmoneyFlowManager.REQUEST_CODE_PAYMENT && resultCode == RESULT_CANCELED) {
+            showAlert("Payment Cancelled");
+        }
+    }
+    //--------------------------------------------
+    private class GetHashesFromServerTask extends AsyncTask<String, String, String> {
+        private ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setMessage("Please wait...");
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... postParams) {
+
+            String merchantHash = "";
+            try {
+                //TODO Below url is just for testing purpose, merchant needs to replace this with their server side hash generation url
+                URL url = new URL("https://test.payu.in");
+
+                String postParam = postParams[0];
+
+                byte[] postParamsByte = postParam.getBytes("UTF-8");
+                Log.d(TAG, "doInBackground: process");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setRequestProperty("Content-Length", String.valueOf(postParamsByte.length));
+                conn.setDoOutput(true);
+                conn.getOutputStream().write(postParamsByte);
+
+                InputStream responseInputStream = conn.getInputStream();
+                StringBuffer responseStringBuffer = new StringBuffer();
+                byte[] byteContainer = new byte[1024];
+                for (int i; (i = responseInputStream.read(byteContainer)) != -1; ) {
+                    responseStringBuffer.append(new String(byteContainer, 0, i));
+                }
+
+                JSONObject response = new JSONObject(responseStringBuffer.toString());
+
+                Iterator<String> payuHashIterator = response.keys();
+                while (payuHashIterator.hasNext()) {
+                    String key = payuHashIterator.next();
+                    Log.d(TAG, " IT doInBackground: " + key);
+                    switch (key) {
+                        /**
+                         * This hash is mandatory and needs to be generated from merchant's server side
+                         *
+                         */
+                        case "payment_hash":
+                            merchantHash = response.getString(key);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return merchantHash;
+        }
+
+        @Override
+        protected void onPostExecute(String merchantHash) {
+            super.onPostExecute(merchantHash);
+
+            progressDialog.dismiss();
+            payNowButton.setEnabled(true);
+
+            if (merchantHash.isEmpty() || merchantHash.equals("")) {
+                Toast.makeText(MainActivity.this, "Could not generate hash", Toast.LENGTH_SHORT).show();
+            } else {
+                mPaymentParams.setMerchantHash(merchantHash);
+                PayUmoneyFlowManager.startPayUMoneyFlow(mPaymentParams, MainActivity.this, R.style.AppTheme_default, mAppPreference.isOverrideResultScreen());
+
+            }
+        }
+    }
+    private void showAlert(String msg){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setMessage(msg);
+        alertDialog.setCancelable(false);
+        alertDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        alertDialog.show();
+    }
+
+
+
+}
